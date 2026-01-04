97fe5a93816305ca89e3ab4d87a6a49b4a35f164
Feven777 <fevhai749@gmail.com>
2026-01-04 09:30:29 +0300

    config: add dataset sampling for safe embedding generation

---
+++ src/vectore_store.py
@@ -0,0 +1,13 @@
+from langchain.vectorstores import Chroma
+from .config import VECTOR_STORE_DIR
+
+
+def build_vector_store(chunks, metadatas, embedding_model):
+    vector_store = Chroma.from_texts(
+        texts=chunks,
+        metadatas=metadatas,
+        embedding=embedding_model,
+        persist_directory=VECTOR_STORE_DIR
+    )
+    vector_store.persist()
+    return vector_store
